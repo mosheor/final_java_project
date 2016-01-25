df56e7d72cfad5d2aa0468bac57118a7c2c41755
ben <benmazliach@gmail.com>
2016-01-26 00:27:35 +0200

    client

--- client/src/model/CommonModel.java
+++ client/src/model/CommonModel.java
@@ -3,6 +3,7 @@ package model;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
